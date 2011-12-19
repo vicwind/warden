1a96fda9269cc82897d7a341b428ad0908badbfa
Victor Huang <victor@weatherbill.com>
2011-12-19 01:30:52 -0800

    remove the debugger in code

--- project/test_project/features/buying_guide_steps.rb
+++ project/test_project/features/buying_guide_steps.rb
@@ -1,7 +1,6 @@
 Given /^i am on the buying guide page/ do
   puts Warden::APP_ENV
   #puts  @warden_session.current_scenario
-  debugger
   visit("/bg/init/?inst_id=217&impl_id=300&locale=en_us")
   page.should have_content('It will only take a few minutes to complete.')
   # "need_help.png"
