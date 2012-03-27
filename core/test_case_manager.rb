133a9b0d0c1d18d3639e4ae3bab1b21e7b2d50c6
Victor Huang <victor@climate.com>
2012-03-26 23:34:03 -0700

    update the test caes manager to have more features

--- core/test_case_manager.rb
+++ core/test_case_manager.rb
@@ -1,6 +1,9 @@
 require 'yaml'
 #Provied the basic interface and tools needed to implment a real Test Case Manager
 #most of the methods in this class need to be imlpemented in the sub-calss
+
+WARDEN_PROJECT_ROOT_PATH = "#{ENV['WARDEN_HOME']}/projects"
+
 class AbstractTestCaseManager
 
   def initialize
@@ -9,7 +12,7 @@ class AbstractTestCaseManager
   end
 
   def get_next_tc_id_from_store()
-    
+
   end
 
   def register_scenario(project_name, scenario)
@@ -17,31 +20,31 @@ class AbstractTestCaseManager
   end
 
   def update_scenario(project_name, scenario)
-
+    raise Exception.new("You need to implement this methods in sub-class")
   end
 
   def delete_scenario(project, scenario)
-
+    raise Exception.new("You need to implement this methods in sub-class")
   end
 
   def tc_id_to_run_path(tc_id)
-
+    raise Exception.new("You need to implement this methods in sub-class")
   end
 
   def register_all()
-
+    raise Exception.new("You need to implement this methods in sub-class")
   end
 
   def find_all_features_and_scenarios_by_project(project_name)
-
+    raise Exception.new("You need to implement this methods in sub-class")
   end
 
   def find_all_scenarios_by_feature(feature)
-
+    raise Exception.new("You need to implement this methods in sub-class")
   end
 
   def find_all_features_by_project(project_name)
-
+    raise Exception.new("You need to implement this methods in sub-class")
   end
 
 end
@@ -50,7 +53,6 @@ class YAMLStoreTestCaseManager < AbstractTestCaseManager
 
   class << self
     attr_accessor :test_case_map_path
-    
   end
   #class instance variable, in placed of class variable
   @test_case_map_path = "#{ENV["WARDEN_HOME"]}/etc/test_case_map.yml"
@@ -62,8 +64,6 @@ class YAMLStoreTestCaseManager < AbstractTestCaseManager
     build_scenario_look_up_map() #it will initialize @scenario_look_up_map
     #puts @scenario_look_up_map.to_yaml
   end
-
-
 
   #params:
   # project_name: string, the name of the project the scenario belonges to
@@ -136,6 +136,18 @@ class YAMLStoreTestCaseManager < AbstractTestCaseManager
         scenario_data_hash['scenario_name']]
   end
 
+  #update the internal scenario look up map, which will be use to find and locate
+  #scenario data by project, feature and scenario name
+  #Params:
+  #  project_name: String, the project's name
+  #  scenario_data_hash: an hash of format:
+  #    #   scenario_data_hash = {
+  #     'id' => unique id
+  #     'feature_name' => string
+  #     'scenario_name' => string
+  #     'line_number' => string
+  #   }
+  #Return: None
   def update_scenario_look_up_map(project_name, scenario_data_hash)
     unless @scenario_look_up_map.has_key? project_name
       @scenario_look_up_map[project_name] = {}
@@ -152,6 +164,21 @@ class YAMLStoreTestCaseManager < AbstractTestCaseManager
         ][scenario_data_hash['scenario_name']] = scenario_data_hash
   end
 
+  #Build the scenario look up map when the test case manager is initialized
+  #the map is then used for look up scenarios by different criteria
+  #Return: a nested hashes of the form
+  # {
+  #   "project_name1" =>
+  #     "feature_name1" =>
+  #       "scneario_name1" => scenario_data_hash
+  #       "scneario_name2" => scenario_data_hash
+  #       ....
+  #   "project_name2" =>
+  #     "feature_name1" =>
+  #       "scneario_name1" => scenario_data_hash
+  #       "scneario_name2" => scenario_data_hash
+  #   ....
+  # }
   def build_scenario_look_up_map
     @scenario_look_up_map = {}
     @tc_id_map[:tc_map].each do |tc_id, test_case_data|
@@ -165,13 +192,24 @@ class YAMLStoreTestCaseManager < AbstractTestCaseManager
     end
   end
 
+  #load the test case map from the yaml file
+  #Return: the test case map hash
   def load_yml_test_case_map()
     YAML::load_file(self.class.test_case_map_path)
   end
 
+  #save the test case map to the yaml file, it will back up the old one
+  #and save the memeory version of the map to a file
   def save_yml_test_case_map()
     FileUtils.mv(self.class.test_case_map_path, self.class.test_case_map_path + ".tmp_bak")
     File.open(self.class.test_case_map_path, "w"){ |file| file.puts(@tc_id_map.to_yaml)} if @tc_id_map
+  end
+
+  #find all of the project name exists in store
+  #return: an array of project name string; an empty array,
+  #  if these isn't any project
+  def find_all_projects()
+    @scenario_look_up_map.keys
   end
 
   #params:
@@ -182,7 +220,7 @@ class YAMLStoreTestCaseManager < AbstractTestCaseManager
     if @scenario_look_up_map.has_key? project_name
        features = @scenario_look_up_map[project_name]
        if features.has_key? feature_name
-         features[feature_name]
+         features[feature_name].dup
        end
     end
   end
@@ -204,7 +242,7 @@ class YAMLStoreTestCaseManager < AbstractTestCaseManager
       project_name, featuren_name, scenario_name, line_number =
         @tc_id_map[:tc_map][tc_id]
 
-      "'#{ENV['WARDEN_HOME']}/projects/#{project_name}/features/#{featuren_name}'" + ' ' +
+      "'#{WARDEN_PROJECT_ROOT_PATH}/#{project_name}/features/#{featuren_name}'" + ' ' +
         '-n ' + scenario_name.inspect
     end
   end
@@ -214,13 +252,14 @@ class YAMLStoreTestCaseManager < AbstractTestCaseManager
   #return:
   #  the scenario data array
   def get_scenario_data_by_id(tc_id)
-    @tc_id_map[:tc_map][tc_id] if @tc_id_map[:tc_map].has_key? tc_id
+    @tc_id_map[:tc_map][tc_id].dup if @tc_id_map[:tc_map].has_key? tc_id
   end
 
   ###############
   ##Class Methods
   ###############
   #
+  #Create a new empty test case map file and save it to disk
   def self.create_new_test_case_map()
     new_test_case_map = {
       :next_available_tc_id => 1,
