28e0b6aa3d969b248c8c664a870d2b1bbb31b100
Victor Huang <victor@climate.com>
2012-06-18 23:56:01 -0700

    enhance seeds to be able to detect exists test case

--- warden_web/db/seeds.rb
+++ warden_web/db/seeds.rb
@@ -32,13 +32,22 @@ test_cases_info[:tc_map].each do |tc_id, tc|
   #   })
   # end
 
-  TestCase.create({
+  target_tc = TestCase.find(:all, :conditions => {
     name: tc[2],
     feature_name: tc[1],
-    feature_file_path: "./#{tc[0]}/#{tc[1]}",
-    register_at: Time.now,
-    tc_id: tc_id,
-    warden_project: warden_project
+    warden_project_id: warden_project.id
   })
+
+  if target_tc.empty?
+    TestCase.create({
+      name: tc[2],
+      feature_name: tc[1],
+      feature_file_path: "./#{tc[0]}/#{tc[1]}",
+      register_at: Time.now,
+      tc_id: tc_id,
+      warden_project: warden_project
+    })
+    puts "Test case '#{warden_project.name}:#{tc[1]}:#{tc[2]}' at './#{tc[0]}/#{tc[1]}' has been registered into DB."
+  end
 
 end
